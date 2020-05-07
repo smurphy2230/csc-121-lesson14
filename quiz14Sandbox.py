c1819aba721b982293788187485dd5a3bda3b1e7
smurphy2230 <smurphy2230@gmail.com>
2020-05-07 15:06:43 -0400

    ready for final

--- quiz14Sandbox.py
+++ quiz14Sandbox.py
@@ -1,8 +1,51 @@
-def main():
-    num_cats = get_cats()
-    print("Number of cats: ", num_cats)
+ #  def main():
+ #      num_cats = get_cats()
+ #      print("Number of cats: ", num_cats)
+ #  
+ #  def get_cats():
+ #      n_cats = int(input("How many cats do you have? "))
+ #      return n_cats
+ #  main()
+ # 
 
-def get_cats():
-    n_cats = int(input("How many cats do you have? "))
-    return n_cats
-main()
+#  def main():
+#       num_cats, num_dogs = get_pets()
+#       print("Number of cats: ", num_cats)
+#       print("Number of dogs: ", num_dogs)
+#  
+#  def get_pets():
+#      n_cats = int(input("Cats? "))
+#      n_dogs = int(input("Dogs? "))
+#      return n_cats, n_dogs
+#  
+#  main()
+
+#  def main():
+#      letter = "p"
+#      letter = change_letter()
+#      print(letter)
+#  
+#  def change_letter():
+#      letter_entered = "k"
+#      return letter_entered
+#  
+#  main()
+
+#  numbers = [-2, -2, 1, 4, 3]
+#  s = 0
+#  for value in numbers:
+#      s += value
+#  print(s)
+
+#  numbers1 = [2, 5]
+#  numbers2 = [7, 1]
+#  numbers3 = numbers1 + numbers2
+#  print(numbers3)
+
+list1 = [2, 5, 7, 6, 4]
+list2 = list1[2:]
+list3 = list1[-3:]
+list4 = list1[-3:5]
+print(list2)
+print(list3)
+print(list4)
